8f4ef07652a67859e1fd3db3649cafbd3864a13b
xenhoxi <leonjerineck@gmail.com>
2024-10-28 09:28:04 +0100

    ex06

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,2 +1,3 @@
 def ft_filter(func, iterable):
-    print(filter.__doc__)
+	"""filter(function or None, iterable) --> filter object\n\nReturn an iterator yielding those items of iterable for which function(item)\nis true. If function is None, return the items that are true."""
+	
