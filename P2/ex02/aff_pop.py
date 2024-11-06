066c8ea7b31a9a4b80442387bb7c1d553a937168
leon <leonjerineck@gmail.com>
2024-11-06 14:42:34 +0100

    P2ex03 inc

--- P2/ex02/aff_pop.py
+++ P2/ex02/aff_pop.py
@@ -1,36 +1,38 @@
 from load_csv import load
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter
-import numpy as np
+
 
 def to_integers(x):
-	if x.endswith('k'):
-		x = float(x[:-1]) * 1_000
-	elif x.endswith('M'):
-		x= float(x[:-1]) * 1_000_000
-	elif x.endswith('B'):
-		x = float(x[:-1]) * 1_000_000_000
-	else:
-		x = x
-	return x
+    if x.endswith('k'):
+        x = float(x[:-1]) * 1_000
+    elif x.endswith('M'):
+        x = float(x[:-1]) * 1_000_000
+    elif x.endswith('B'):
+        x = float(x[:-1]) * 1_000_000_000
+    else:
+        x = x
+    return x
 
 
 def main():
-	try:
-		data = load("../population_total.csv")
-		data.set_index('country', inplace=True)
-		Yemen = data.loc['Yemen'].apply(to_integers).plot(label='Yemen')
-		France = data.loc['France'].apply(to_integers).plot(label='France')
-		Yemen.yaxis.set_major_formatter(EngFormatter())
-		France.yaxis.set_major_formatter(EngFormatter())
-		plt.title("Population Projections")
-		plt.xlabel("Year")
-		plt.ylabel("Population")
-		plt.legend()
-		plt.show()
-	except KeyboardInterrupt as error_msg:
-		return
+    try:
+        data = load("../data_csv/population_total.csv")
+        data.set_index('country', inplace=True)
+        Yemen = data.loc['Yemen'][:-50]
+        Yemen = Yemen.apply(to_integers).plot(label='Yemen')
+        France = data.loc['France'][:-50]
+        France = France.apply(to_integers).plot(label='France')
+        Yemen.yaxis.set_major_formatter(EngFormatter())
+        France.yaxis.set_major_formatter(EngFormatter())
+        plt.title("Population Projections")
+        plt.xlabel("Year")
+        plt.ylabel("Population")
+        plt.legend()
+        plt.show()
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- P2/ex02/load_csv.py
+++ P2/ex02/load_csv.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 
 def load(path: str):
-	try:
-		csv = pd.read_csv(path)
-		print(f"Loading dataset of dimensions {csv.shape}")
-		return csv
-	except FileNotFoundError as error:
-		print(error)
-	except IsADirectoryError as error:
-		print(error)
+    try:
+        csv = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {csv.shape}")
+        return csv
+    except FileNotFoundError as error:
+        print(error)
+    except IsADirectoryError as error:
+        print(error)
