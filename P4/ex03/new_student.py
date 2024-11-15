e498cfd2d34afe0a8205abdd8c885162cde04bc1
leon <leonjerineck@gmail.com>
2024-11-15 11:17:11 +0100

    P4 ex0

--- P4/ex03/new_student.py
+++ P4/ex03/new_student.py
@@ -17,11 +17,16 @@ class Student:
 
     def __init__(self, **kwags):
         try:
-            print(len(kwags))
-            if not kwags['name'] and not kwags['surname'] and len(kwags) != 2:
-                raise TypeError("")
+            unexpected = set(kwags) - {'name', 'surname'}
+            unexpected = list(unexpected)
+            if unexpected:
+                msg = "Student.__init__() got an unexpected keyword argument"
+                raise TypeError(f"{msg} {', '.join(unexpected)}")
+            if not kwags['name'] or not kwags['surname']:
+                raise TypeError("Student.__init__() missing keyword argument")
             self.name = kwags['name']
             self.surname = kwags['surname']
             self.id = generate_id()
-        except (Exception, TypeError):
-            print("Error")
+            self.login = self.name[0] + self.surname[:7]
+        except (Exception, TypeError) as err:
+            print("TypeError:", err)
--- P4/ex03/tester.py
+++ P4/ex03/tester.py
@@ -1,5 +1,12 @@
 from new_student import Student
 
 
-student = Student(name="Edward", surname="agle", id="fsfd")
+student = Student(name="Edward", surname="agle")
 print(student)
+print()
+
+student = Student(name="Edward", surname="agle", id='noid')
+try:
+    print(student)
+except Exception as err_msg:
+    print(err_msg)
